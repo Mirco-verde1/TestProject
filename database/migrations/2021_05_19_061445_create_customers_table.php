e55f53e23aefc7758f37d3f3cdd78c4a4b32a21d
Mirco Verderosa <verderosamircowork@gmail.com>
2021-05-19 08:53:03 +0200

    scaffolding

---
+++ database/migrations/2021_05_19_061445_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('surname',100);
+            $table->string('email')->unique();
+            $table->string('phone',16);
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+
+     <div class="header">
+         <div class="list-info-header">
+            <ul>
+                <li> <a href=""> CHI SIAMO </a></li>
+                <li><a href=""> SERVIZI </a></li>
+                <li><a href=""> CONTATTI </a></li>
+            </ul>
+         </div>
+     </div>
+
+   </div>
+  </div>
