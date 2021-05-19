12e6e48329b3a7f77b57d300242ea9c417b3bb6c
Mirco Verderosa <verderosamircowork@gmail.com>
2021-05-19 12:13:29 +0200

    added route 'appointment' in header and card container

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,27 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=
 
-    , initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Autofficina</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        , initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Autofficina</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
-</head>
-<body>
+    </head>
+    <body>
 
-    <!-- header part -->
-     @include('partials.header')
+        <!-- header part -->
+        @include('partials.header')
 
-     <!-- Start jumbotron -->
-     <div class="container-fluid">
-         <div class="row">
-             <div class="jumbo-background">
+        <!-- Start jumbotron -->
+        <div class="container-fluid">
+            <div class="row">
+                <div class="jumbo-background">
 
-             </div>
-         </div>
-     </div>
-</body>
-</html>
+                    <!-- card in jumbotron -->
+                    <div class="card-container">
+
+                        <div class="content-card">
+
+                        </div>
+
+                        <div class="content-card">
+
+                        </div>
+
+                        <div class="content-card">
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,10 @@
      <div class="header">
          <div class="list-info-header">
             <ul>
-                <li> <a href=""> CHI SIAMO </a></li>
+                <li><a href=""> CHI SIAMO </a></li>
                 <li><a href=""> SERVIZI </a></li>
                 <li><a href=""> CONTATTI     </a></li>
+                <li><a href="{{route('appointment')}}"> PRENOTA APPUNTAMENTO</a></li>
             </ul>
          </div>
      </div>
