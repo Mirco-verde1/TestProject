62af30d023f0fc67a762494d582c207f37df7c34
Mirco Verderosa <verderosamircowork@gmail.com>
2021-05-20 15:05:04 +0200

    return json from ApiController ordered from last id

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Customer;
+use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
     public function __invoke()
     {
 
-        $data = Customer::all()->groupBy('id','desc')->first();
+        $data = DB::table('customers')->orderBy('id', 'desc')->get();
 
 
         return response()->json([
